484bb25c0474fef0a60cb090d105269745d78d3b
MateuszCharysz <charyszek@interia.pl>
2023-02-24 00:30:23 +0100

    proper gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,39 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+//help
+const log = console.log;
+// log(galleryItems);
+
+//DOM elements
+const gallery = document.querySelector('.gallery');
+// log(gallery);
+
+//callback/functions
+const createItem = item => {
+  let itemMarkup = `
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+`;
+  return itemMarkup;
+};
+
+const drawGallery = galleryItems.map(item => createItem(item)).join('');
+gallery.innerHTML = drawGallery;
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionSelector: event => event.firstElementChild,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+//Events
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  // log(event.target);
+  event.target.lightbox;
+});
 
 console.log(galleryItems);
