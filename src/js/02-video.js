1edfb007e21409dee1bfb7680084cd25abca24dc
MateuszCharysz <charyszek@interia.pl>
2023-02-26 17:39:13 +0100

    1st try vimeo window

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,31 @@
-import * as vimeoPlayer from '@vimeo/player';
+import * as player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const throttle = require('lodash.throttle');
 
 //help
 const log = console.log;
-
+// const _.throttle(() => {func}, 1000)
 //DOM
 const windowPlayer = document.querySelector('#vimeo-player');
 log(windowPlayer);
 
 //callback
-const onPlay = () => {}
+const onPlay = event => {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+      // seconds = the current playback position
+    })
+    .catch(function (error) {
+      console.log('shit went south');
+      // an error occurred
+    });
+};
+
+windowPlayer.player.on(
+  'timeupdate',
+  _.throttle(() => {
+    onPlay;
+  }, 1000),
+);
