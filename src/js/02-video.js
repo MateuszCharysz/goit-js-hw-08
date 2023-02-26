33b67bdea57ba3cbd0e4907fbb27e34359e38926
MateuszCharysz <charyszek@interia.pl>
2023-02-26 16:46:55 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import * as vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+//help
+const log = console.log;
+
+//DOM
+const windowPlayer = document.querySelector('#vimeo-player');
+log(windowPlayer);
+
+//callback
+const onPlay = () => {}
